4828da03b7c472968b39a375aca1d6d32238ec7f
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-06-10 19:01:58 -0300

    termina primer punto

--- hello.py
+++ hello.py
@@ -1,2 +1,17 @@
 # Hola mundo
 print ("Hola Python")
+
+"""
+Este es un comentario 
+con comillas dobles
+en varias lineas
+"""
+
+'''
+Este tambien 
+es un comentario
+en comillas simples
+'''
+
+# Consultar el tipo de dato
+print(type('Hola Python'))  # Tipo 'str'
