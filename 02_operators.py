a8e5dcdc6ed8c5bbb402948751e73704648f28d9
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-06-11 21:16:20 -0300

    operadores comparativos

---
+++ 02_operators.py
@@ -0,0 +1,17 @@
+# Operadores 
+
+### Operadores Aritméticos ###
+
+# Operaciones con enteros
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)  #Resto
+print(10 // 3)  #Redondea 
+print(2 ** 3)   #Multiplo
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+# Operaciones con cadenas de texto
+print("Hola " + "Python " + "¿Qué tal?")
+print("Hola " + str(5))
