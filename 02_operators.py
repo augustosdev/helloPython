3e4a5409bfb19723127f4c867716aa16f567dd63
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-06-12 07:26:25 -0300

    operadores

--- 02_operators.py
+++ 02_operators.py
@@ -15,3 +15,29 @@ print(2 ** 3 + 3 - 7 / 1 // 4)
 # Operaciones con cadenas de texto
 print("Hola " + "Python " + "¿Qué tal?")
 print("Hola " + str(5))
+
+# Operaciones con enteros
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(4 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+# Operaciones con cadenas de texto
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+
+# Operadores Logicos
+print(2<3 or 'git'!='hello')
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" > "Python")
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not (3 > 4))
