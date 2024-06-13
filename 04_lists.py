0e6f55bb11ecc2fe31ab98a22c699dc9c22c376e
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-06-13 08:29:04 -0300

    lists

---
+++ 04_lists.py
@@ -0,0 +1,19 @@
+###  LISTAS  ###
+
+my_list = list()
+
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [35, 24, 62, 30, 30, 15]
+
+print(my_list)
+
+my_other_list = [35, 1.69, 'Samuel', 'Augusto']
+
+print(type(my_list))
+print(type(my_other_list))
+
+print(my_other_list[1])
+print(my_other_list[-1])
