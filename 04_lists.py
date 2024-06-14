41bb2d19b8b062060a2244421bccf68e0ffbd93f
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-06-13 21:09:23 -0300

    final listas

--- 04_lists.py
+++ 04_lists.py
@@ -17,3 +17,48 @@ print(type(my_other_list))
 
 print(my_other_list[1])
 print(my_other_list[-1])
+
+# Creación, inserción, actualización y eliminación
+
+my_other_list.append("MoureDev")
+print(my_other_list)
+
+my_other_list.insert(1, "Rojo")
+print(my_other_list)
+
+my_other_list[1] = "Azul"
+print(my_other_list)
+
+my_other_list.remove("Azul")
+print(my_other_list)
+
+my_list.remove(30)
+print(my_list)
+
+print(my_list.pop())
+print(my_list)
+
+my_pop_element = my_list.pop(2)
+print(my_pop_element)
+print(my_list)
+
+del my_list[2]
+print(my_list)
+
+# Operaciones con listas
+
+my_new_list = my_list.copy()
+
+my_list.clear()
+print(my_list)
+print(my_new_list)
+
+my_new_list.reverse()
+print(my_new_list)
+
+my_new_list.sort()
+print(my_new_list)
+
+# Sublistas
+
+print(my_new_list[1:3])
