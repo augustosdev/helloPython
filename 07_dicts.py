b6b28440006c03d6960edb4d07d6d32fc08ab958
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-06-15 20:23:14 -0300

    dict

--- 07_dicts.py
+++ 07_dicts.py
@@ -1,7 +1,27 @@
 # Diccionario
 
+my_dict1 = dict()
+print(type(my_dict1))
+my_dict1 = {'name':'Augusto'}
+print(my_dict1)
+print(type(my_dict1))
+
 my_dict = dict()
+my_other_dict = {}
+
 print(type(my_dict))
-my_dict = {'name':'Augusto'}
+print(type(my_other_dict))
+
+my_other_dict = {"Nombre": "Augusto",
+                 "Apellido": "Alvarado", "Edad": 35, 1: "Python"}
+
+my_dict = {
+    "Nombre": "Augusto",
+    "Apellido": "Alvarado",
+    "Edad": 35,
+    "Lenguajes": {"Python", "Swift", "Kotlin"},
+    1: 1.69
+}
+
+print(my_other_dict)
 print(my_dict)
-print(type(my_dict))
