5446fcc877ea21954c90054b7c8cb82bdf835e86
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-06-26 07:33:31 -0300

    funciones terminadas

--- 10_functions.py
+++ 10_functions.py
@@ -15,3 +15,19 @@ def suma_return(valor1, valor2):
 
 valor_suma = suma_return(12, 24) # Se guarda en una variable el retorno
 print(valor_suma)
+
+def print_name(name, surname):
+    print(f'{name} {surname}')
+
+print_name(surname='Alvarado', name='Samuel')
+
+def nombres(name, surname = 'n/s', age = 'sin edad'): # Se le asigna un parametro por defecto
+    print(f'{name} {surname} {age}')
+
+nombres('Samuel')
+
+def func_parametro_libre(*texts):
+    for text in texts:
+        print(text.upper())
+
+func_parametro_libre('hola', 'parametro', 'libre')
