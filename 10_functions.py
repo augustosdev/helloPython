ec7a4deedaf7da6d86329120cdaeb1b9ff68250d
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-06-25 21:39:30 -0300

    funciones

---
+++ 10_functions.py
@@ -0,0 +1,17 @@
+# Functions
+
+def my_function():
+    print('Esta es una funcion')
+
+my_function()
+
+def sumas(numero1, numero2):
+    print(numero1 + numero2)
+
+sumas(25, 16)
+
+def suma_return(valor1, valor2):
+    return valor1 + valor2
+
+valor_suma = suma_return(12, 24) # Se guarda en una variable el retorno
+print(valor_suma)
