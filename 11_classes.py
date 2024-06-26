802ff071d6f843b3430e0018f58f34b4af52a352
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-06-26 19:08:07 -0300

    clases, propiedad privada

--- 11_classes.py
+++ 11_classes.py
@@ -6,12 +6,17 @@ class MyPerson:
 print(MyPerson)
 
 class Person:
-    def __init__(self, name, surname):
-        self.fullname = f'{name} {surname}'
+    def __init__(self, name, surname, alias = 'Sin alias'):
+        self.fullname = f'{name} {surname} ({alias})'  # Es una propiedad Publica
+        self.__name = name # Es una propiedad privada
+
+    def get_name(self):
+        return self.__name    
 
     def walk(self):
         print(f'{self.fullname} esta caminando')
 
 my_person = Person('Samuel', 'Alvarado')
 print(my_person.fullname)
+print(my_person.get_name())
 my_person.walk()
