32d51c7560cc4f4af39e03246c869f0e908e4766
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-06-27 21:52:58 -0300

    captura de errores

--- 12_exceptions.py
+++ 12_exceptions.py
@@ -3,7 +3,7 @@
 number_one = 5
 number_two = 2
 
-#number_two = '2'
+number_two = '2'
 
 # Try except
 
@@ -29,3 +29,25 @@ else:
 finally:
     # Se ejecuta siempre
     print('La ejecucion continua')
+
+
+# Excepciones de tipo
+
+try:
+    print(number_one + number_two)
+    print('No se ha producido un error')
+except ValueError:
+    print('Se ha producido un error de tipo ValueError')
+except TypeError:
+    print('Se ha producido un error TypeError')
+
+
+# Captura la informacion de la excepcion
+
+try:
+    print(number_one + number_two)
+    print('No se ha producido un error')
+except ValueError as error_valor:
+    print(error_valor)
+except TypeError as error_tipo:
+    print('El error es este: ', error_tipo)
