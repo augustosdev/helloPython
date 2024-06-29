f347188eeabd9d654580c2b35d7a04e032734630
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-06-29 17:51:32 -0300

    datetime

---
+++ intermediate/00_dates.py
@@ -0,0 +1,20 @@
+# Dates
+
+from datetime import datetime
+
+now = datetime.now()
+
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.minute)
+    print(date.second)
+    print(date.timestamp())
+
+print_date(now)
+
+year_2023 = datetime(2025, 1, 15)
+
+print_date(year_2023)
