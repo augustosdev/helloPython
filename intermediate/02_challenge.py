76b7d055cc33d774d4e97443a259a57575e082be
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-07-04 08:28:23 -0300

    anagrama

--- intermediate/02_challenge.py
+++ intermediate/02_challenge.py
@@ -23,3 +23,15 @@ def fizz_buzz():
 
 fizz_buzz()
     
+
+"""
+¿ES UN ANAGRAMA?
+Escribe una función que reciba dos palabras (String) y retorne
+verdadero o falso (Bool) según sean o no anagramas.
+- Un Anagrama consiste en formar una palabra reordenando TODAS
+  las letras de otra palabra inicial.
+- NO hace falta comprobar que ambas palabras existan.
+- Dos palabras exactamente iguales no son anagrama.
+"""
+
+
