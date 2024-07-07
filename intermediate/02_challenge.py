9711619c94df6fa6392da3a08bf703698142e4bc
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-07-07 13:16:02 -0300

    numero primo

--- intermediate/02_challenge.py
+++ intermediate/02_challenge.py
@@ -66,4 +66,25 @@ def fibonacci():
     else:
         print('Termino la secuencia')
 
-fibonacci()
+fibonacci() 
+
+
+"""
+¿ES UN NÚMERO PRIMO?
+Escribe un programa que se encargue de comprobar si un número es o no primo.
+Hecho esto, imprime los números primos entre 1 y 100.
+"""
+
+
+def numero_primo():
+    for number in range(1, 101):
+        if number >= 2:
+            is_divisible = False
+            for index in range(2, number):
+                if number % index == 0:
+                    is_divisible = True
+                    break
+            if not is_divisible:
+                print(number)    
+
+numero_primo()
