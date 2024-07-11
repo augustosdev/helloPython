d8c2de5f3df82862de24562638ed5df80bf210ac
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-07-11 20:54:06 -0300

    .txt file

--- intermediate/06_file_handling.py
+++ intermediate/06_file_handling.py
@@ -1,2 +1,29 @@
 # Manejo de ficheros
 
+import os
+
+# .txt file
+
+txt_file = open('intermediate/my_file.txt', 'w+') 
+
+txt_file.write('Mi nombre es Samuel\nMi apellido Alvarado\ntengo 35\nestoy aprendiendo Python')
+
+#print(txt_file.read())
+print(txt_file.read(10))
+print(txt_file.readline())
+print(txt_file.readline())
+for line in txt_file.readlines():
+    print(line)
+
+txt_file.write('\nAunque tambien me gusta JS')
+print(txt_file.readline())
+
+txt_file.close()
+
+with open('intermediate/my_file.txt', 'a') as  my_other_file:
+    my_other_file.write('\nY SQL')
+
+#os.remove('intermediate/my_file.txt')
+
+
+
