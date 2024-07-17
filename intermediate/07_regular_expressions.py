3b235b276205465b45096a3277fa84b5a904a88e
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-07-17 08:23:42 -0300

    match re

--- intermediate/07_regular_expressions.py
+++ intermediate/07_regular_expressions.py
@@ -2,6 +2,10 @@
 
 import re
 
+# Match
+
 my_string = 'Esta es la leccion numero 7: Expresiones regulares'
 my_other_string = 'Esta no es la leccion numero 6: Manejo de ficheros'
 
+match = re.match('Esta es la leccion', my_string, re.I)
+print(match)
