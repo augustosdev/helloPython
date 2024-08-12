19c63c8dcc273cc9ecc40acd79e8988dfbec86fc
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-08-12 08:21:22 -0300

    pip install

--- intermediate/08_python_package_manager.py
+++ intermediate/08_python_package_manager.py
@@ -2,9 +2,24 @@
 
 #PIP  https://pypi.org/
 
-import numpy
+import numpy        #pip install numpy
 
 print(numpy.version.version)
 
 numpy_array = numpy.array([12, 23, 15, 33, 35])
 print(type(numpy_array))
+
+import pandas       #pip install pandas
+
+# pip list
+# pip unistall pandas
+# pip show numpy
+
+import requests 
+
+response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=151")
+print(response)
+print(response.status_code)
+print(response.json())
+
+
